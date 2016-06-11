ecd9791d8973c8b9cf2980231db1ef1d7dc19587
motleytech <motleytechnet@gmail.com>
2016-06-11 16:44:41 -0700

    adding gravity train picture and fixing home link

--- updateWebsite.sh
+++ updateWebsite.sh
@@ -1,5 +1,6 @@
 rm -rf motleytechnet/output/*
 cd motleytechnet
 pelican content
-ssh root@motleytech.net 'rm -rf /website/*'
-scp -r output/* root@motleytech.net:/website/
+ssh root@motleytech.net 'rm -rf /website2; mkdir /website2'
+scp -r output/* root@motleytech.net:/website2/
+ssh root@motleytech.net 'rm -rf /website; mv /website2 /website'
